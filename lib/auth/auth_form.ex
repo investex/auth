e2c8f2315b5a35419ed773e84c3d7a1730ce6870
ryantaylor <ryan.taylor9@gmail.com>
2019-10-19 00:44:34 -0400

    Move flows to their own modules and delegate.

--- lib/auth/auth_form.ex
+++ lib/auth/auth_form.ex
@@ -1,5 +1,6 @@
-defmodule Auth.RegistrationForm do
+defmodule Auth.AuthForm do
   use Ecto.Schema
+  use Rop
 
   embedded_schema do
     field :email, :string
@@ -10,5 +11,12 @@ defmodule Auth.RegistrationForm do
     form
     |> Ecto.Changeset.cast(params, [:email, :password])
     |> Ecto.Changeset.validate_required([:email, :password])
+  end
+
+  def validate_input(params) do
+    %Auth.AuthForm{}
+    |> Auth.AuthForm.changeset(params)
+    |> Ecto.Changeset.apply_action(:insert)
+    >>> bind(Map.from_struct)
   end
 end
