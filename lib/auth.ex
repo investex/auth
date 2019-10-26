a2418668600cb2fa8cf6ae2e76019cfbd23bbd24
ryantaylor <ryan.taylor9@gmail.com>
2019-10-25 21:05:11 -0400

    Update delegates to point to the right places.

--- lib/auth.ex
+++ lib/auth.ex
@@ -1,4 +1,4 @@
 defmodule Auth do
-  defdelegate register(params), to: Auth.Registration, as: :process
-  defdelegate authenticate(params), to: Auth.Authentication, as: :process
+  defdelegate register(params), to: Auth.Sagas.Registration, as: :process
+  defdelegate authenticate(params), to: Auth.Sagas.Authentication, as: :process
 end
