78d3f55a7adaee812fe5a4ef71cc3782a27ef6a1
ryantaylor <ryan.taylor9@gmail.com>
2019-10-25 21:04:04 -0400

    Move form into folder.

---
+++ lib/auth/forms/auth_form.ex
@@ -0,0 +1,27 @@
+defmodule Auth.Forms.AuthForm do
+  use Ecto.Schema
+  use Rop
+
+  import Utils.Rop
+
+  alias Auth.Forms.AuthForm
+
+  embedded_schema do
+    field :email, :string
+    field :password, :string
+  end
+
+  def changeset(form, params \\ %{}) do
+    form
+    |> Ecto.Changeset.cast(params, [:email, :password])
+    |> Ecto.Changeset.validate_required([:email, :password])
+  end
+
+  def validate_input(%{user: user} = params) do
+    %AuthForm{}
+    |> AuthForm.changeset(user)
+    |> Ecto.Changeset.apply_action(:insert)
+    >>> bind(Map.from_struct)
+    >>> put_back(params, :user)
+  end
+end
